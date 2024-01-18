0d04ce225cbe9d2a6f9b8cb8156aaa773d1206d9
Nana Kwame Amagyei <nanakwameamagyei@Nanas-MacBook-Pro.local>
2024-01-18 15:22:46 +0000

    task 0 with instruction_t and stack_t

--- main.c
+++ main.c
@@ -16,6 +16,10 @@
 stack_t* declarestack_t(int capacity) {
     stack_t* stack = malloc(sizeof(stack_t));
 
+	
+
+	
+
     if (stack == NULL) {
         printf("Memory allocation failed. Exiting.\n");
         exit(EXIT_FAILURE);
@@ -41,6 +45,12 @@ void freestack_t(stack_t* stack) {
 int main(int argc, char* argv[]) {
 	FILE* file = fopen(argv[1], "r");
 	stack_t* mystack_t = declarestack_t(MAX_STACK_SIZE);
+	instruction_t instructions[] = {
+        {"push", push},
+        {"pall", pall},
+
+        {NULL, NULL} // Terminate the array with NULL values
+    };
 	char opcode[50];
 	char arg[50];
 	int line_number = 0;
@@ -58,32 +68,27 @@ int main(int argc, char* argv[]) {
 	}
 
 
-	while (fscanf(file, "%s", opcode) != EOF)
-	{
-		line_number++;
-		if (strcmp(opcode, "push") == 0)
-		{
-			if (fscanf(file, "%s", arg) != 1)
-			{
-				printf("L%d: usage: push integer\n", line_number);
-				freestack_t(mystack_t);
-				fclose(file);
-				exit(EXIT_FAILURE);
-			}
-			push(mystack_t, arg, line_number);
-		}
-		else if (strcmp(opcode, "pall") == 0)
-		{
-			pall(mystack_t);
-		}
-		else
-		{
-			printf("L%d: Unknown opcode: %s\n", line_number, opcode);
-			freestack_t(mystack_t);
-			fclose(file);
-			exit(EXIT_FAILURE);
-		}
-	}
+	while (fscanf(file, "%s", opcode) != EOF) {
+        line_number++;
+
+        // Search for the opcode in the instructions array
+        int i = 0;
+        while (instructions[i].opcode != NULL) {
+            if (strcmp(opcode, instructions[i].opcode) == 0) {
+                // Execute the corresponding function
+                instructions[i].f(&mystack_t, line_number);
+                break;
+            }
+            i++;
+        }
+
+        if (instructions[i].opcode == NULL) {
+            printf("L%d: Unknown opcode: %s\n", line_number, opcode);
+            freeStack(mystack_t);
+            fclose(file);
+            exit(EXIT_FAILURE);
+        }
+    }
 
 	fclose(file);
 	freestack_t(mystack_t);
